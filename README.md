97d9a29bc411f83010c164c33c159072b775a97f
liuyib <1656081615@qq.com>
2020-10-20 20:21:41 +0800

    add 'T#109 convert-sorted-list-to-binary-search-tree'

--- README.md
+++ README.md
@@ -18,6 +18,7 @@
 |228|[汇总区间](https://leetcode-cn.com/problems/summary-ranges/)|⭐⭐|[JavaScript](./javascript/medium/105-从前序与中序遍历序列构造二叉树.js)|`Array`|
 |203|[移除链表元素](https://leetcode-cn.com/problems/remove-linked-list-elements/)|⭐|[JavaScript](./javascript/easy/203-移除链表元素.js)|`Linked List`|
 |143|[重排链表](https://leetcode-cn.com/problems/reorder-list/)|⭐⭐|[JavaScript](./javascript/medium/143-重排链表.js)|`Linked List`|
+|109|[有序链表转换二叉搜索树](https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree/)|⭐⭐|[JavaScript](./javascript/medium/109-有序链表转换二叉搜索树.js)|`Linked List`, `Binary Search Tree`|
 |105|[从前序与中序遍历序列构造二叉树](https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/)|⭐⭐|[JavaScript](./javascript/medium/105-从前序与中序遍历序列构造二叉树.js)|`Array`, `Binary Tree`|
 |79|[单词搜索](https://leetcode-cn.com/problems/word-search/)|⭐⭐|[JavaScript](./javascript/medium/79-单词搜索.js)|`Backtracking`|
 |22|[括号生成](https://leetcode-cn.com/problems/generate-parentheses/)|⭐⭐|[JavaScript](./javascript/medium/22-括号生成.js)|`Backtracking`|
---
+++ javascript/medium/109-有序链表转换二叉搜索树.js
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode.cn id=109 lang=javascript
+ *
+ * 作者：liuyib <https://github.com/liuyib>
+ * 日期：2020-10-20
+ *
+ * [109] 有序链表转换二叉搜索树
+ *
+ * https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree/description/
+ *
+ * @tags 'Linked List' 'Binary Search Tree'
+ *
+ * 给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+ *
+ * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+ *
+ * 示例:
+ *
+ * 给定的有序链表： [-10, -3, 0, 5, 9],
+ *
+ * 一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+ *
+ * ⁠     0
+ * ⁠    / \
+ * ⁠  -3   9
+ * ⁠  /   /
+ * ⁠-10  5
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+/**
+ * @param {ListNode} head
+ * @return {TreeNode}
+ */
+var sortedListToBST = function (head) {
+  const arr = [];
+
+  // 转为数组
+  while (head) {
+    arr.push(head.val);
+    head = head.next;
+  }
+
+  // 思路：每次取数组中间的元素作为根节点，然后对左右的元素集合递归操作即可
+  function buildTree(arr) {
+    const len = arr.length;
+    const mid = Math.floor(len / 2);
+
+    if (arr[mid] === undefined) {
+      return null;
+    }
+
+    const root = new TreeNode(arr[mid]);
+
+    if (mid === 0) {
+      return root;
+    }
+
+    root.left = buildTree(arr.slice(0, mid));
+    root.right = buildTree(arr.slice(mid + 1));
+
+    return root;
+  }
+
+  return buildTree(arr);
+};
+// @lc code=end
