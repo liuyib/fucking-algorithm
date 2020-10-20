09380ede8e5d16e4e48523d95beb132712318bbf
liuyib <1656081615@qq.com>
2020-10-20 17:12:38 +0800

    add 'T#143 reorder-list'

--- README.md
+++ README.md
@@ -16,6 +16,7 @@
 |203|[移除链表元素](https://leetcode-cn.com/problems/remove-linked-list-elements/)|⭐|[JavaScript](./javascript/easy/203-移除链表元素.js)|`Linked List`|
 |228|[汇总区间](https://leetcode-cn.com/problems/summary-ranges/)|⭐⭐|[JavaScript](./javascript/medium/105-从前序与中序遍历序列构造二叉树.js)|`Array`|
 |203|[移除链表元素](https://leetcode-cn.com/problems/remove-linked-list-elements/)|⭐|[JavaScript](./javascript/easy/203-移除链表元素.js)|`Array`|
+|143|[重排链表](https://leetcode-cn.com/problems/reorder-list/)|⭐⭐|[JavaScript](./javascript/medium/143-重排链表.js)|`Linked List`|
 |105|[从前序与中序遍历序列构造二叉树](https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/)|⭐⭐|[JavaScript](./javascript/medium/105-从前序与中序遍历序列构造二叉树.js)|`Array`, `Binary Tree`|
 |79|[单词搜索](https://leetcode-cn.com/problems/word-search/)|⭐⭐|[JavaScript](./javascript/medium/79-单词搜索.js)|`Backtracking`|
 |22|[括号生成](https://leetcode-cn.com/problems/generate-parentheses/)|⭐⭐|[JavaScript](./javascript/medium/22-括号生成.js)|`Backtracking`|
---
+++ javascript/medium/143-重排链表.js
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=143 lang=javascript
+ *
+ * [143] 重排链表
+ *
+ * https://leetcode-cn.com/problems/reorder-list/description/
+ *
+ * @tags 'Linked List'
+ *
+ * 给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+ * 将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+ *
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ *
+ * 示例 1:
+ *
+ * 给定链表 1->2->3->4, 重新排列为 1->4->2->3.
+ *
+ * 示例 2:
+ *
+ * 给定链表 1->2->3->4->5, 重新排列为 1->5->2->4->3.
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function (head) {
+  const order = function (head) {
+    if (!head) return [];
+
+    let _head = head;
+    let _curr = head;
+
+    // 找到倒数第二个
+    while (_curr.next && _curr.next.next) {
+      _curr = _curr.next;
+    }
+
+    if (_curr.next) {
+      const tail = new ListNode(_curr.next.val);
+      _curr.next = null;
+      tail.next = _head.next;
+      _head.next = tail;
+
+      if (_head.next && _head.next.next) {
+        order(_head.next.next);
+      }
+    }
+  };
+
+  order(head);
+};
+
+// 测试，先用数组构建出链表
+const inputs = [1, 2, 3, 4, 5];
+let head = null;
+let curr = null;
+
+inputs.forEach((input) => {
+  const newNode = new ListNode(input);
+  if (head === null) {
+    curr = head = newNode;
+  } else {
+    curr.next = newNode;
+    curr = curr.next;
+  }
+});
+
+// 重新排序
+reorderList(head);
+
+// 输出结果
+let result = "";
+while (head !== null) {
+  result += `${head.val}${head.next === null ? "" : "->"}`;
+  head = head.next;
+}
+console.log(result);
+// @lc code=end
