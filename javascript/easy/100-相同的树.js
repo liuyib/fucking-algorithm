6fe11b743ac7b7d9970542d35c6f162de72f7988
liuyib <1656081615@qq.com>
2020-11-17 10:59:20 +0800

    add 'T#100 same-tree'

---
+++ javascript/easy/100-相同的树.js
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=javascript
+ *
+ * [100] 相同的树
+ *
+ * https://leetcode-cn.com/problems/same-tree/description/
+ *
+ * @tags 'Binary Tree' 'DFS'
+ *
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ *
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ *
+ * 示例 1:
+ *
+ * 输入:       1         1
+ * ⁠         / \       / \
+ * ⁠        2   3     2   3
+ *
+ * ⁠       [1,2,3],   [1,2,3]
+ *
+ * 输出: true
+ *
+ * 示例 2:
+ *
+ * 输入:      1          1
+ * ⁠         /           \
+ * ⁠        2             2
+ *
+ * ⁠       [1,2],     [1,null,2]
+ *
+ * 输出: false
+ *
+ *
+ * 示例 3:
+ *
+ * 输入:       1         1
+ * ⁠         / \       / \
+ * ⁠        2   1     1   2
+ *
+ * ⁠       [1,2,1],   [1,1,2]
+ *
+ * 输出: false
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+  if (p === null && q === null) {
+    return true;
+  } else if (p === null || q === null) {
+    return false;
+  } else if (p.val !== q.val) {
+    return false;
+  }
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+// @lc code=end
