6d5067103d78968517033024de2f4c8714f8b430
liuyib <1656081615@qq.com>
2020-11-17 16:10:16 +0800

    add 'T#513 find-bottom-left-tree-value'

--- README.md
+++ README.md
@@ -21,6 +21,7 @@
 |695|[岛屿的最大面积](https://leetcode-cn.com/problems/max-area-of-island/)|⭐⭐|[JavaScript](./javascript/medium/695-岛屿的最大面积.js)|`DFS`||
 |599|[两个列表的最小索引总和](https://leetcode-cn.com/problems/minimum-index-sum-of-two-lists/)|⭐|[JavaScript](./javascript/easy/599-两个列表的最小索引总和.js)|`Array`, `Hash Table`||
 |560|[和为K的子数组](https://leetcode-cn.com/problems/subarray-sum-equals-k/)|⭐⭐|[JavaScript](./javascript/medium/560-和为k的子数组.js)|`Array`, `Hash Table`, `前缀和`||
+|513|[找树左下角的值](https://leetcode-cn.com/problems/find-bottom-left-tree-value/)|⭐⭐|[JavaScript](./javascript/medium/513-找树左下角的值.js)|`Binary Tree`, `DFS`, `BFS`||
 |394|[字符串解码](https://leetcode-cn.com/problems/decode-string/)|⭐⭐|[JavaScript](./javascript/medium/394-字符串解码.js)|`Stack`, `DFS`||
 |237|[删除链表中的节点](https://leetcode-cn.com/problems/delete-node-in-a-linked-list/)|⭐|[JavaScript](./javascript/easy/237-删除链表中的节点.js)|`Linked List`||
 |228|[汇总区间](https://leetcode-cn.com/problems/summary-ranges/)|⭐⭐|[JavaScript](./javascript/medium/105-从前序与中序遍历序列构造二叉树.js)|`Array`||
---
+++ javascript/medium/513-找树左下角的值.js
@@ -0,0 +1,94 @@
+/*
+ * @lc app=leetcode.cn id=513 lang=javascript
+ *
+ * [513] 找树左下角的值
+ *
+ * https://leetcode-cn.com/problems/find-bottom-left-tree-value/description/
+ *
+ * @tags 'Binary Tree' 'DFS' 'BFS'
+ *
+ * 给定一个二叉树，在树的最后一行找到最左边的值。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ *
+ * ⁠   2
+ * ⁠  / \
+ * ⁠ 1   3
+ *
+ * 输出:
+ * 1
+ *
+ * 示例 2:
+ *
+ * 输入:
+ *
+ * ⁠       1
+ * ⁠      / \
+ * ⁠     2   3
+ * ⁠    /   / \
+ * ⁠   4   5   6
+ * ⁠      /
+ * ⁠     7
+ *
+ * 输出:
+ * 7
+ *
+ * 注意: 您可以假设树（即给定的根节点）不为 NULL。
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * BFS：改造 BFS，每层从右向左遍历
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findBottomLeftValue = function (root) {
+  const queue = [root];
+  let node = root;
+
+  while (queue.length) {
+    node = queue.shift();
+    if (node.right) queue.push(node.right);
+    if (node.left) queue.push(node.left);
+  }
+
+  return node.val;
+};
+
+/**
+ * DFS
+ */
+var findBottomLeftValue = function (root) {
+  let ret = 0;
+  let max = 0; // 最大深度
+  let cur = 0; // 当前深度
+
+  var dfs = function (node) {
+    if (!node) return;
+
+    cur++;
+
+    if (cur > max) {
+      max = cur;
+      ret = node.val;
+    }
+
+    dfs(node.left);
+    dfs(node.right);
+    cur--;
+  };
+
+  dfs(root);
+
+  return ret;
+};
+// @lc code=end
