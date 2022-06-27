47d6423f4422812207c25eac92f0b8ff8b41d58e
liuyibo <liuyibo@xylink.com>
2022-06-28 00:29:46 +0800

    review 'T#203 移除链表元素', 两种方法(有无虚拟头结点)

--- javascript/easy/203-移除链表元素.js
+++ javascript/easy/203-移除链表元素.js
@@ -33,28 +33,60 @@
  * @param {number} val
  * @return {ListNode}
  */
-var removeElements = function (head, val) {
-  const virtualHead = new ListNode(null);
-  virtualHead.next = head;
 
-  let prev = virtualHead;
+// 无虚拟头节点实现
+var removeElements = function (head, val) {
+  while (head !== null && head.val === val) {
+    const delNode = head;
+
+    head = delNode.next;
+    delNode.next = null;
+  }
+
+  if (head === null) {
+    return null;
+  }
+
+  let prev = head;
 
   while (prev.next !== null) {
     if (prev.next.val === val) {
-      prev.next = prev.next.next;
+      const delNode = prev.next;
+
+      prev.next = delNode.next;
+      delNode.next = null;
     } else {
       prev = prev.next;
     }
   }
 
-  return virtualHead.next;
+  return head;
 };
 
-// 递归实现
-var removeElements = function (head, val) {
-  if (head === null) return null;
+// // 有虚拟头结点实现
+// var removeElements = function (head, val) {
+//   const dummyHead = new ListNode(null, head);
+//   let prev = dummyHead;
 
-  head.next = removeElements(head.next, val);
-  return head.val === val ? head.next : head;
-};
+//   while (prev.next !== null) {
+//     if (prev.next.val === val) {
+//       const delNode = prev.next;
+
+//       prev.next = delNode.next;
+//       delNode.next = null;
+//     } else {
+//       prev = prev.next;
+//     }
+//   }
+
+//   return dummyHead.next;
+// };
+
+// // 递归实现
+// var removeElements = function (head, val) {
+//   if (head === null) return null;
+
+//   head.next = removeElements(head.next, val);
+//   return head.val === val ? head.next : head;
+// };
 // @lc code=end
