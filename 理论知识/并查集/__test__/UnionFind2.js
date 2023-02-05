1e411e826a6f952ff8659e32854f3d8797a041f6
liuyibo <liuyibo@xylink.com>
2023-02-05 17:05:50 +0800

    test(并查集): 添加第一版和第二版并查集的基准测试

--- 理论知识/并查集/__test__/UnionFind2.js
+++ 理论知识/并查集/__test__/UnionFind2.js
@@ -1,8 +1,9 @@
 const assert = require('node:assert/strict');
 const test = require('node:test');
-const UnionFind = require('../UnionFind2');
+const UnionFind1 = require('../UnionFind1');
+const UnionFind2 = require('../UnionFind2');
 
-const uf = new UnionFind(10);
+const uf = new UnionFind2(10);
 
 test('测试 UnionFind2.js - size, find, isConnected 方法', () => {
   assert.strictEqual(uf.size(), 10);
@@ -27,3 +28,29 @@ test('测试 UnionFind2.js - union 方法', () => {
   assert.strictEqual(uf.isConnected(0, 3), false);
   assert.strictEqual(uf.isConnected(0, 4), false);
 });
+
+function testUnionFind(uf, count) {
+  for (let i = 0; i < count; i++) {
+    uf.union(Math.random(), Math.random());
+  }
+
+  for (let i = 0; i < count; i++) {
+    uf.isConnected(Math.random(), Math.random());
+  }
+}
+
+const size = 100_000_000;
+const count = 10000;
+const uf1 = new UnionFind1(size);
+const uf2 = new UnionFind2(size);
+
+test('UnionFind1.js - 基准测试（Benchmark）', () => {
+  testUnionFind(uf1, count);
+});
+test('UnionFind2.js - 基准测试（Benchmark）', () => {
+  testUnionFind(uf2, count);
+});
+
+// 基准测试结论：
+// - 当并查集的 size 较小时，UnionFind1 和 UnionFind2 性能差距不大
+// - 当并查集的 size 特别大时（node v18.0.0，size 一亿以上），两者才能看出明显的性能差距
