29792c28bfa9d2a8102ea4b5f50547565b7dbed5
liuyib <1656081615@qq.com>
2023-05-31 01:43:18 +0800

    add T#662

---
+++ javascript/medium/662-二叉树最大宽度.js
@@ -0,0 +1,103 @@
+/*
+ * @lc app=leetcode.cn id=662 lang=javascript
+ *
+ * [662] 二叉树最大宽度
+ *
+ * https://leetcode.cn/problems/maximum-width-of-binary-tree/description/
+ *
+ * algorithms
+ * Medium (43.65%)
+ * Likes:    569
+ * Dislikes: 0
+ * Total Accepted:    89.3K
+ * Total Submissions: 204.6K
+ * Testcase Example:  '[1,3,2,5,3,null,9]'
+ *
+ * 给你一棵二叉树的根节点 root ，返回树的 最大宽度 。
+ *
+ * 树的 最大宽度 是所有层中最大的 宽度 。
+ *
+ *
+ *
+ * 每一层的 宽度 被定义为该层最左和最右的非空节点（即，两个端点）之间的长度。将这个二叉树视作与满二叉树结构相同，两端点间会出现一些延伸到这一层的
+ * null 节点，这些 null 节点也计入长度。
+ *
+ * 题目数据保证答案将会在  32 位 带符号整数范围内。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,3,2,5,3,null,9]
+ * 输出：4
+ * 解释：最大宽度出现在树的第 3 层，宽度为 4 (5,3,null,9) 。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,3,2,5,null,null,9,6,null,7]
+ * 输出：7
+ * 解释：最大宽度出现在树的第 4 层，宽度为 7 (6,null,null,null,null,null,7) 。
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = [1,3,2,5]
+ * 输出：2
+ * 解释：最大宽度出现在树的第 2 层，宽度为 2 (3,2) 。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点的数目范围是 [1, 3000]
+ * -100 <= Node.val <= 100
+ *
+ *
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var widthOfBinaryTree = function (root) {
+  if (!root) return root;
+
+  let ans = 1n;
+  const q = [[root, 1n]];
+
+  while (q.length) {
+    const size = q.length;
+
+    for (let i = 0; i < size; i++) {
+      const [node, pos] = q.shift();
+
+      if (node.left) q.push([node.left, pos * 2n]);
+      if (node.right) q.push([node.right, pos * 2n + 1n]);
+    }
+
+    if (q.length) {
+      const newAns = q[q.length - 1][1] - q[0][1] + 1n;
+      ans = ans > newAns ? ans : newAns;
+    }
+  }
+
+  return ans;
+};
+// @lc code=end
