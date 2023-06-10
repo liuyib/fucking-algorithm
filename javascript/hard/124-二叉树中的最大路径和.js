681c38c9479849917fca10ac0a0372fd17e255af
liuyib <1656081615@qq.com>
2023-06-11 00:00:51 +0800

    add T#124

---
+++ javascript/hard/124-二叉树中的最大路径和.js
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode.cn id=124 lang=javascript
+ *
+ * [124] 二叉树中的最大路径和
+ *
+ * https://leetcode.cn/problems/binary-tree-maximum-path-sum/description/
+ *
+ * algorithms
+ * Hard (45.29%)
+ * Likes:    1974
+ * Dislikes: 0
+ * Total Accepted:    324.4K
+ * Total Submissions: 716.3K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 二叉树中的 路径 被定义为一条节点序列，序列中每对相邻节点之间都存在一条边。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个
+ * 节点，且不一定经过根节点。
+ *
+ * 路径和 是路径中各节点值的总和。
+ *
+ * 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,2,3]
+ * 输出：6
+ * 解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [-10,9,20,null,null,15,7]
+ * 输出：42
+ * 解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点数目范围是 [1, 3 * 10^4]
+ * -1000 <= Node.val <= 1000
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function (root) {
+  let ans = -Infinity;
+
+  const dfs = function (node) {
+    if (!node) return 0;
+
+    let left = Math.max(dfs(node.left), 0);
+    let right = Math.max(dfs(node.right), 0);
+
+    ans = Math.max(ans, node.val + left + right);
+
+    return node.val + Math.max(left, right);
+  };
+
+  dfs(root);
+
+  return ans;
+};
+// @lc code=end
